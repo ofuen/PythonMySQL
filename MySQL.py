bd10754f85b7f22433b4cb7cd60c2a8be9e9128a
oscar <oscar@o2webdev.com>
2018-11-25 12:17:32 -0500

    First commit :snake:

---
+++ MySQL.py
@@ -0,0 +1,17 @@
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","root","Localhost123","test")
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() methodself.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() methodself.
+data = cursor.fetchone()
+print "Database version : %s " % data
+
+# disconnect from server
+db.close()
